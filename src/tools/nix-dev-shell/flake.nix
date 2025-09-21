99188384cb5095e843db3d1cc8195030de048944
mike <h@solfunmeme.com>
2025-09-21 17:17:18 +0000

    feat(rust): CRQ-016 - Automated commit for CRQ-016 changes

--- src/tools/nix-dev-shell/flake.nix
+++ src/tools/nix-dev-shell/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "rustc dev shell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
 
   outputs =
     {
