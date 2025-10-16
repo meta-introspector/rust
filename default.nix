e6c1b92d0abaa3f64032d6662cbcde980c826ff2
mike <h@solfunmeme.com>
2025-10-16 18:13:20 +0000

    this might be our new solana platform on arm again

--- default.nix
+++ default.nix
@@ -7,8 +7,13 @@ pkgs.stdenv.mkDerivation {
 
   src = ./.; # Source is the current directory
 
+  nativeBuildInputs = [
+    pkgs.python3 # Add python3 to the build environment
+  ];
+
   buildPhase = ''
     echo "Starting Rust build using x.py..."
+    rm -f config.toml # Remove existing config.toml
     python x.py build
   '';
 
