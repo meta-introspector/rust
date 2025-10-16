ab72068304c490213399f57b9fc8bde298b2c7b4
mike <h@solfunmeme.com>
2025-10-16 11:28:33 +0000

    Update flake.nix for standalone build and revert default.nix

--- flake.nix
+++ flake.nix
@@ -2,22 +2,35 @@
   description = "Rust (meta-introspector fork)";
 
   inputs = {
-    # self.url = "github:meta-introspector/rust?ref=feature/CRQ-016-nixify";
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
+    rust-overlay.url = "github:meta-introspector/rust-overlay?ref=feature/CRQ-016-nixify";
   };
 
-  outputs = { self, nixpkgs, flake-utils } :
+  outputs = { self, nixpkgs, flake-utils, rust-overlay } :
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
       in
       rec {
-        packages.default = pkgs.callPackage ./default.nix { }; # Assuming a default.nix for the actual build
-        devShells.default = pkgs.mkShell {
+        packages.default = pkgs.callPackage ./default.nix { }; # Provides the source
+        devShells.default = with pkgs; mkShell {
+          name = "rust-src-dev-shell";
           packages = [
-            packages.default
+            rustc
+            cargo
           ];
+          # Add other nativeBuildInputs and buildInputs if needed for development
+          nativeBuildInputs = [
+            binutils cmake ninja pkg-config python3 git curl cacert patchelf nix
+          ];
+          buildInputs = [
+            openssl glibc.out glibc.static
+          ];
+          RUSTC_ICE = "0";
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath [
+            stdenv.cc.cc.lib
+          ]}";
         };
       });
 }
