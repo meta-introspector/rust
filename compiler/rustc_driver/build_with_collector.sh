e3ec611e05f4d4524689e6ed79bdf9c7ead970a3
mike dupont <mike.dupont@introspector.local>
2026-01-03 16:22:02 -0500

    help

--- compiler/rustc_driver/build_with_collector.sh
+++ compiler/rustc_driver/build_with_collector.sh
@@ -11,4 +11,5 @@ cd rustc_driver
 CFG_RELEASE_CHANNEL=dev RUSTC_INSTALL_BINDIR=/usr/bin RUSTC=../../target/debug/working_usage_collector cargo build
 
 echo "=== Results ==="
-ls -la usage_data/
+ls -la ../../usage_data/ | wc -l
+echo "Files created in ../../usage_data/"
