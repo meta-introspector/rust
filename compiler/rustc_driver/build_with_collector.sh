1c432fbd0d4703b9433c0c7ceb87f99f6e262729
mike dupont <mike.dupont@introspector.local>
2026-01-03 15:29:59 -0500

    update

--- compiler/rustc_driver/build_with_collector.sh
+++ compiler/rustc_driver/build_with_collector.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Build rustc_driver using our usage_collector as the rustc driver
-echo "=== Building rustc_driver with usage_collector ==="
+# Build rustc_driver using our introspector-collector
+echo "=== Building rustc_driver with introspector-collector ==="
 
 # Build our collector first
-rustc usage_collector.rs -o usage_collector
+cd ../introspector-collector
+cargo build --bin working_usage_collector
+cd ../rustc_driver
 
 # Use our collector as RUSTC to build current directory
-CFG_RELEASE_CHANNEL=dev RUSTC_INSTALL_BINDIR=/usr/bin RUSTC=/mnt/data1/nix/vendor/rust/cargo2nix/submodules/rust/compiler/rustc_driver/usage_collector cargo build
+CFG_RELEASE_CHANNEL=dev RUSTC_INSTALL_BINDIR=/usr/bin RUSTC=../../target/debug/working_usage_collector cargo build
 
 echo "=== Results ==="
 ls -la usage_data/
